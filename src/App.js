a491bca29b80dda9c15b382f9da573bfc46e9e8d
kodyyuen <kody.yuen@gmail.com>
2023-05-22 19:15:56 -0400

    fixed routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import './App.css';
-import Home from './components/home';
-import Navigation from './components/navigation';
-import About from './components/about';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import Projects from './components/projects';
-import Contact from './components/contact';
-import Footer from './components/footer';
+import "./App.css";
+import Home from "./components/home";
+import Navigation from "./components/navigation";
+import About from "./components/about";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import Projects from "./components/projects";
+import Contact from "./components/contact";
+import Footer from "./components/footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const Body = () => {
+  return (
+    <>
+      <Home />
+      <About />
+      <Projects />
+      <Contact />
+    </>
+  );
+};
 
 function App() {
   return (
-    <>
-      <Navigation/>
-      <Home/>
-      <About/>
-      <Projects/>
-      <Contact/>
-      <Footer/>
-    </>
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route index element={<Body />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
